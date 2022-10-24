23f60e5ddaf9cf6dbb9ef7f13cf9db3184b7f8ae
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-10-24 12:04:58 +0100

    Added footer section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,9 +3,9 @@ import image from "../assets/image.jpeg";
 
 function Content() {
   return (
-    <div className="flex flex-col justify-start p-0 gap-14">
+    <div className="flex flex-col justify-start p-0 gap-14 mt-4">
       <div className="flex flex-col items-center p-0 gap-6 isolate">
-        <img alt="profile_png" src={image} className="flex flex-col justify-center items-center p-0 rounded-[275px] w-[88px] h-[88px]" />
+        <img alt="profile_png" src={image} className="flex flex-col justify-center items-center p-0 rounded-[275px] w-[100px] h-[100px]" />
         <div className="flex flex-row gap-2 p-0 justify-center h-8">
           <h2 className="text-xl font-sans font-[700] text-gray-900 items-center">Confidence James</h2>
         </div>
