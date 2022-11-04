c305087a26a3709f56ad41c549cb811531a764d5
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-11-04 12:33:18 +0100

    bug: made interface responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import CheckBox from "./CheckBox";
 
 function Contact() {
   return (
-    <div className="bg-[#ffffff] sm:block lg:flex flex-col items-center justify-center px-0 pt-16 pb-24 gap-16 relative">
+    <div className="bg-[#ffffff] md:flex flex-col items-center justify-center px-0 pt-16 pb-24 gap-16 relative">
       <div className="container flex flex-row justify-center items-center py-0 px-8 gap-16">
         <div className="flex flex-col justify-center items-center p-0">
           <div className="flex flex-col items-start p-0 gap-12">
@@ -12,13 +12,13 @@ function Contact() {
               <h1 className="text-[#101828] font-[600] text-[36px] tracking-[-0.02em] leading-[44px]">Contact Me</h1>
               <h1 className="text-[#475467] font-[400] text-[20px] leading-[30px]">Hi there, contact me to ask about anything you have in mind</h1>
             </div>
-            <form className="flex flex-col items-start p-0 gap-8 w-full">
-              <div className="flex flex-col items-start p-0 gap-6">
-                <div className="flex flex-row items-start p-0 gap-6">
-                  <Input forHtml="first_nam " label="First Name" placeholder="Enter your first name" width="60" type="text" id="first_name" />
-                  <Input forHtml="last_name" label="Last Name" placeholder="Enter your last name" width="60" type="text" id="last_name" />
+            <form className="flex flex-col p-0 gap-8 w-full">
+              <div className="flex flex-col md:items-start p-0 gap-6">
+                <div className="md:flex flex-row p-0 gap-6 w-full">
+                  <Input forHtml="first_name" label="First Name" placeholder="Enter your first name" type="text" id="first_name" />
+                  <Input forHtml="last_name" label="Last Name" placeholder="Enter your last name" type="text" id="last_name" mt="5" />
                 </div>
-                <Input forHtml="email" label="Email" placeholder="your@email.com" width="full" type="email" id="email" />
+                <Input forHtml="email" label="Email" placeholder="your@email.com" type="email" id="email" />
                 <TextArea id="message" />
                 <CheckBox type="check" />
               </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
-function Input({ label, forHtml, placeholder, width, type, id }) {
+function Input({ label, forHtml, placeholder, type, id, mt }) {
   return (
-    <div className="flex flex-col items-start p-0 gap-[6px] w-full">
+    <div className={`md:flex flex-col items-start p-0 gap-[6px] w-full md:m-0 mt-${mt}`}>
       <label htmlFor={forHtml} className="font-[500] text-[14px] leading-5 text-[#344054]">
         {label}
       </label>
       <input
-        className={`box-border bg-[#ffffff] border border-solid border-[#D0D5DD] px-[14px] py-[10px] drop-shadow-1x rounded-lg w-${width} placeholder:text-[#667085] self-stretch`}
+        className={`box-border bg-[#ffffff] border border-solid border-[#D0D5DD] px-[14px] py-[10px] drop-shadow-1x rounded-lg w-full placeholder:text-[#667085] self-stretch`}
         placeholder={placeholder}
         type={type}
         id={id}
