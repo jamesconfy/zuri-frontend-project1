d705790613367dc08d2e6d7c4438d0644d7eb0a8
James Confidence <63427518+jamesconfy@users.noreply.github.com>
2022-11-07 14:52:18 +0100

    bug: Made the whole buttons to be the link:

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,15 +2,15 @@
 
 function Button({ title, address, id }) {
   return (
-    <div className="flex flex-row justify-center items-center p-0 gap-3 drop-shadow-2x w-full">
-      <button
-        id={id}
-        className="rounded-lg box-border border-[1px] border-solid border-[#EAECF0] flex flex-row justify-center items-center px-8 py-6 gap-2 bg-gray-200 lg:w-4/5 w-full"
-      >
-        <a href={address} target="_blank" rel="noreferrer" className="pointer">
+    <div className="flex flex-row justify-center items-center p-0 gap-3 drop-shadow-2x w-full lg:w-4/5">
+      <a href={address} target="_blank" rel="noreferrer" className="pointer w-full">
+        <button
+          id={id}
+          className="rounded-lg box-border border-[1px] border-solid border-[#EAECF0] flex flex-row justify-center items-center px-8 py-6 gap-2 bg-gray-200 w-full"
+        >
           <h2 className="flex text-center text-lg font-sans font-[500] text-[#101828] items-center">{title}</h2>
-        </a>
-      </button>
+        </button>
+      </a>
     </div>
   );
 }
